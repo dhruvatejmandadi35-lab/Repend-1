d1a18808cc2b12e492800d2e997dcd618b060920
dhruvatejmandadi35-lab <dhruvatejmandadi35@gmail.com>
2026-05-26 07:04:41 +0530

    Restore server.listen (complete the startup)

--- server.js
+++ server.js
@@ -266,3 +266,4 @@ const server = http.createServer(async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Repend running at http://localhost:${PORT}`));
